375c0941b03cd64690e95ccdba81b7ab2da92441
Mohammed Rafique <mohammedrafique23@gmail.com>
2021-07-05 12:30:39 +0530

    Added blog.php

---
+++ blog.php
@@ -0,0 +1,343 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="assets/css/bootstrap..min.css">
+    <link rel="stylesheet" href="assets/css/boxicons.min.css">
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/odometer.min.css">
+    <link rel="stylesheet" href="assets/css/meanmenu.min.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/nice-select.min.css">
+    <link rel="stylesheet" href="assets/css/viewer.min.css">
+    <link rel="stylesheet" href="assets/css/slick.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <title>Enduv</title>
+    <link rel="icon" type="image/png" href="assets/img/favicon.png">
+</head>
+
+<body>
+
+    <div class="preloader">
+        <div class="loader">
+            <div class="shadow"></div>
+            <div class="box"></div>
+        </div>
+    </div>
+
+
+    <?php include './templates/header.php' ?>
+
+
+    <div class="search-overlay">
+        <div class="d-table">
+            <div class="d-table-cell">
+                <div class="search-overlay-layer"></div>
+                <div class="search-overlay-layer"></div>
+                <div class="search-overlay-layer"></div>
+                <div class="search-overlay-close">
+                    <span class="search-overlay-close-line"></span>
+                    <span class="search-overlay-close-line"></span>
+                </div>
+                <div class="search-overlay-form">
+                    <form>
+                        <input type="text" class="input-search" placeholder="Search here...">
+                        <button type="submit"><i class='bx bx-search-alt'></i></button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="page-title-area item-bg3 jarallax" data-jarallax='{"speed": 0.3}'>
+        <div class="container">
+            <div class="page-title-content">
+                <ul>
+                    <li><a href="index-2.html">Home</a></li>
+                    <li>Blog</li>
+                </ul>
+                <h2>Blog Grid</h2>
+            </div>
+        </div>
+    </div>
+
+
+    <section class="blog-area ptb-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-blog-post mb-30">
+                        <div class="post-image">
+                            <a href="single-blog.html" class="d-block">
+                                <img src="assets/img/blog/1.jpg" alt="image">
+                            </a>
+                            <div class="tag">
+                                <a href="#">Learning</a>
+                            </div>
+                        </div>
+                        <div class="post-content">
+                            <ul class="post-meta">
+                                <li class="post-author">
+                                    <img src="assets/img/user1.jpg" class="d-inline-block rounded-circle mr-2"
+                                        alt="image">
+                                    By: <a href="#" class="d-inline-block">Steven Smith</a>
+                                </li>
+                                <li><a href="#">August 30, 2021</a></li>
+                            </ul>
+                            <h3><a href="single-blog.html" class="d-inline-block">World largest elephant toothpaste
+                                    experiment in 2021</a></h3>
+                            <a href="#" class="read-more-btn">Read More <i class='bx bx-right-arrow-alt'></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-blog-post mb-30">
+                        <div class="post-image">
+                            <a href="single-blog.html" class="d-block">
+                                <img src="assets/img/blog/2.jpg" alt="image">
+                            </a>
+                            <div class="tag">
+                                <a href="#">Education</a>
+                            </div>
+                        </div>
+                        <div class="post-content">
+                            <ul class="post-meta">
+                                <li class="post-author">
+                                    <img src="assets/img/user2.jpg" class="d-inline-block rounded-circle mr-2"
+                                        alt="image">
+                                    By: <a href="#" class="d-inline-block">Lina D'Souja</a>
+                                </li>
+                                <li><a href="#">August 29, 2021</a></li>
+                            </ul>
+                            <h3><a href="single-blog.html" class="d-inline-block">Most Popular Education Posts Of The
+                                    Week 20-26 Aug</a></h3>
+                            <a href="#" class="read-more-btn">Read More <i class='bx bx-right-arrow-alt'></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-blog-post mb-30">
+                        <div class="post-image">
+                            <a href="single-blog.html" class="d-block">
+                                <img src="assets/img/blog/3.jpg" alt="image">
+                            </a>
+                            <div class="tag">
+                                <a href="#">Management</a>
+                            </div>
+                        </div>
+                        <div class="post-content">
+                            <ul class="post-meta">
+                                <li class="post-author">
+                                    <img src="assets/img/user3.jpg" class="d-inline-block rounded-circle mr-2"
+                                        alt="image">
+                                    By: <a href="#" class="d-inline-block">David Malan</a>
+                                </li>
+                                <li><a href="#">August 28, 2021</a></li>
+                            </ul>
+                            <h3><a href="single-blog.html" class="d-inline-block">How to enhance education quality
+                                    management system</a></h3>
+                            <a href="#" class="read-more-btn">Read More <i class='bx bx-right-arrow-alt'></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-blog-post mb-30">
+                        <div class="post-image">
+                            <a href="single-blog.html" class="d-block">
+                                <img src="assets/img/blog/4.jpg" alt="image">
+                            </a>
+                            <div class="tag">
+                                <a href="#">Ideas</a>
+                            </div>
+                        </div>
+                        <div class="post-content">
+                            <ul class="post-meta">
+                                <li class="post-author">
+                                    <img src="assets/img/user5.jpg" class="d-inline-block rounded-circle mr-2"
+                                        alt="image">
+                                    By: <a href="#" class="d-inline-block">David Warner</a>
+                                </li>
+                                <li><a href="#">August 27, 2021</a></li>
+                            </ul>
+                            <h3><a href="single-blog.html" class="d-inline-block">Global education: Ideas for the way
+                                    move forward</a></h3>
+                            <a href="#" class="read-more-btn">Read More <i class='bx bx-right-arrow-alt'></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-blog-post mb-30">
+                        <div class="post-image">
+                            <a href="single-blog.html" class="d-block">
+                                <img src="assets/img/blog/5.jpg" alt="image">
+                            </a>
+                            <div class="tag">
+                                <a href="#">Workforce</a>
+                            </div>
+                        </div>
+                        <div class="post-content">
+                            <ul class="post-meta">
+                                <li class="post-author">
+                                    <img src="assets/img/user6.jpg" class="d-inline-block rounded-circle mr-2"
+                                        alt="image">
+                                    By: <a href="#" class="d-inline-block">Olivar Waise</a>
+                                </li>
+                                <li><a href="#">August 26, 2021</a></li>
+                            </ul>
+                            <h3><a href="single-blog.html" class="d-inline-block">New report reimagines the broader
+                                    education workforce</a></h3>
+                            <a href="#" class="read-more-btn">Read More <i class='bx bx-right-arrow-alt'></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-blog-post mb-30">
+                        <div class="post-image">
+                            <a href="single-blog.html" class="d-block">
+                                <img src="assets/img/blog/6.jpg" alt="image">
+                            </a>
+                            <div class="tag">
+                                <a href="#">Education</a>
+                            </div>
+                        </div>
+                        <div class="post-content">
+                            <ul class="post-meta">
+                                <li class="post-author">
+                                    <img src="assets/img/user2.jpg" class="d-inline-block rounded-circle mr-2"
+                                        alt="image">
+                                    By: <a href="#" class="d-inline-block">Lina D'Souja</a>
+                                </li>
+                                <li><a href="#">August 29, 2021</a></li>
+                            </ul>
+                            <h3><a href="single-blog.html" class="d-inline-block">What’s Going On in This Picture? |
+                                    Jan. 13, 2021</a></h3>
+                            <a href="#" class="read-more-btn">Read More <i class='bx bx-right-arrow-alt'></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-blog-post mb-30">
+                        <div class="post-image">
+                            <a href="single-blog.html" class="d-block">
+                                <img src="assets/img/blog/7.jpg" alt="image">
+                            </a>
+                            <div class="tag">
+                                <a href="#">Learning</a>
+                            </div>
+                        </div>
+                        <div class="post-content">
+                            <ul class="post-meta">
+                                <li class="post-author">
+                                    <img src="assets/img/user1.jpg" class="d-inline-block rounded-circle mr-2"
+                                        alt="image">
+                                    By: <a href="#" class="d-inline-block">Steven Smith</a>
+                                </li>
+                                <li><a href="#">August 30, 2021</a></li>
+                            </ul>
+                            <h3><a href="single-blog.html" class="d-inline-block">Connecting Math and Science to Reading
+                                    and Writing</a></h3>
+                            <a href="#" class="read-more-btn">Read More <i class='bx bx-right-arrow-alt'></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-blog-post mb-30">
+                        <div class="post-image">
+                            <a href="single-blog.html" class="d-block">
+                                <img src="assets/img/blog/8.jpg" alt="image">
+                            </a>
+                            <div class="tag">
+                                <a href="#">Education</a>
+                            </div>
+                        </div>
+                        <div class="post-content">
+                            <ul class="post-meta">
+                                <li class="post-author">
+                                    <img src="assets/img/user2.jpg" class="d-inline-block rounded-circle mr-2"
+                                        alt="image">
+                                    By: <a href="#" class="d-inline-block">Lina D'Souja</a>
+                                </li>
+                                <li><a href="#">August 29, 2021</a></li>
+                            </ul>
+                            <h3><a href="single-blog.html" class="d-inline-block">How to Introduce Meditation to the
+                                    High School</a></h3>
+                            <a href="#" class="read-more-btn">Read More <i class='bx bx-right-arrow-alt'></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-blog-post mb-30">
+                        <div class="post-image">
+                            <a href="single-blog.html" class="d-block">
+                                <img src="assets/img/blog/9.jpg" alt="image">
+                            </a>
+                            <div class="tag">
+                                <a href="#">Management</a>
+                            </div>
+                        </div>
+                        <div class="post-content">
+                            <ul class="post-meta">
+                                <li class="post-author">
+                                    <img src="assets/img/user3.jpg" class="d-inline-block rounded-circle mr-2"
+                                        alt="image">
+                                    By: <a href="#" class="d-inline-block">David Malan</a>
+                                </li>
+                                <li><a href="#">August 28, 2021</a></li>
+                            </ul>
+                            <h3><a href="single-blog.html" class="d-inline-block">Tools for Creating Digital Student
+                                    Portfolios</a></h3>
+                            <a href="#" class="read-more-btn">Read More <i class='bx bx-right-arrow-alt'></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <div class="pagination-area text-center">
+                        <span class="page-numbers current" aria-current="page">1</span>
+                        <a href="#" class="page-numbers">2</a>
+                        <a href="#" class="page-numbers">3</a>
+                        <a href="#" class="page-numbers">4</a>
+                        <a href="#" class="page-numbers">5</a>
+                        <a href="#" class="next page-numbers"><i class='bx bx-chevron-right'></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <?php include './templates/footer.php' ?>
+
+    <div class="go-top "><i class='bx bx-up-arrow-alt'></i></div>
+
+    <script data-cfasync="false " src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js "></script>
+    <script src="assets/js/jquery.min.js "></script>
+    <script src="assets/js/popper.min.js "></script>
+    <script src="assets/js/bootstrap.min.js "></script>
+    <script src="assets/js/owl.carousel.min.js "></script>
+    <script src="assets/js/mixitup.min.js "></script>
+    <script src="assets/js/parallax.min.js "></script>
+    <script src="assets/js/jquery.appear.min.js "></script>
+    <script src="assets/js/odometer.min.js "></script>
+    <script src="assets/js/particles.min.js "></script>
+    <script src="assets/js/meanmenu.min.js "></script>
+    <script src="assets/js/jquery.nice-select.min.js "></script>
+    <script src="assets/js/viewer.min.js "></script>
+    <script src="assets/js/slick.min.js "></script>
+    <script src="assets/js/jquery.magnific-popup.min.js "></script>
+    <script src="assets/js/jquery.ajaxchimp.min.js "></script>
+    <script src="assets/js/form-validator.min.js "></script>
+    <script src="assets/js/contact-form-script.js "></script>
+    <script src="assets/js/main.js "></script>
+</body>
+
+</html>

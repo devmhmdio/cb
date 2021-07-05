3d8c2f1d996b7e1c84ce0a5dfc5b9ff7c38267f1
Mohammed Rafique <mohammedrafique23@gmail.com>
2021-07-05 13:14:14 +0530

    Added contact page

---
+++ contact.php
@@ -0,0 +1,214 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="assets/css/bootstrap..min.css">
+    <link rel="stylesheet" href="assets/css/boxicons.min.css">
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/odometer.min.css">
+    <link rel="stylesheet" href="assets/css/meanmenu.min.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/nice-select.min.css">
+    <link rel="stylesheet" href="assets/css/viewer.min.css">
+    <link rel="stylesheet" href="assets/css/slick.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <title>Enduv</title>
+    <link rel="icon" type="image/png" href="assets/img/favicon.png">
+</head>
+
+<body>
+
+    <div class="preloader">
+        <div class="loader">
+            <div class="shadow"></div>
+            <div class="box"></div>
+        </div>
+    </div>
+
+
+    <?php include './templates/header.php' ?>
+
+
+    <div class="search-overlay">
+        <div class="d-table">
+            <div class="d-table-cell">
+                <div class="search-overlay-layer"></div>
+                <div class="search-overlay-layer"></div>
+                <div class="search-overlay-layer"></div>
+                <div class="search-overlay-close">
+                    <span class="search-overlay-close-line"></span>
+                    <span class="search-overlay-close-line"></span>
+                </div>
+                <div class="search-overlay-form">
+                    <form>
+                        <input type="text" class="input-search" placeholder="Search here...">
+                        <button type="submit"><i class='bx bx-search-alt'></i></button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="page-title-area item-bg2 jarallax" data-jarallax='{"speed": 0.3}'>
+        <div class="container">
+            <div class="page-title-content">
+                <ul>
+                    <li><a href="index-2.html">Home</a></li>
+                    <li>Contact</li>
+                </ul>
+                <h2>Contact Us</h2>
+            </div>
+        </div>
+    </div>
+
+    <section class="contact-info-area pt-100 pb-70">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="contact-info-box mb-30">
+                        <div class="icon">
+                            <i class='bx bx-envelope'></i>
+                        </div>
+                        <h3>Email Us</h3>
+                        
+                        <p><a
+                                href="mailto:hello@learningshala.in"><span
+                                    class="__cf_email__">hello@learningshala.in</span></a>
+                        </p>
+                        <p><a
+                                href="mailto:info@learningshala.in"><span
+                                    class="__cf_email__">info@learningshala.in</span></a>
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="contact-info-box mb-30">
+                        <div class="icon">
+                            <i class='bx bx-map'></i>
+                        </div>
+                        <h3>InternShala</h3>
+                        <p><a href="https://goo.gl/maps/d1oYxhWjQw96Jjid9" target="_blank">
+Laxmi Nagar
+Block S1, Nanakpura, Shakarpur, New Delhi, Delhi 110092
+</a></p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 offset-lg-0 offset-md-3 offset-sm-3">
+                    <div class="contact-info-box mb-30">
+                        <div class="icon">
+                            <i class='bx bx-phone-call'></i>
+                        </div>
+                        <h3>Call Us</h3>
+                        <p><a href="tel:1234567890">+91 99110 06041</a></p>
+                        <p><a href=""></a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="particles-js-circle-bubble-2"></div>
+    </section>
+
+    <section class="contact-area pb-100">
+        <div class="container">
+            <div class="section-title">
+                <span class="sub-title">Contact Us</span>
+                <h2>Drop us Message for any Query</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua.</p>
+            </div>
+            <div class="contact-form">
+                <form id="contactForm">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-12">
+                            <div class="form-group">
+                                <input type="text" name="name" id="name" class="form-control" required
+                                    data-error="Please enter your name" placeholder="Your Name">
+                                <div class="help-block with-errors"></div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-12">
+                            <div class="form-group">
+                                <input type="email" name="email" id="email" class="form-control" required
+                                    data-error="Please enter your email" placeholder="Your Email">
+                                <div class="help-block with-errors"></div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-12">
+                            <div class="form-group">
+                                <input type="text" name="phone_number" id="phone_number" required
+                                    data-error="Please enter your number" class="form-control" placeholder="Your Phone">
+                                <div class="help-block with-errors"></div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-12">
+                            <div class="form-group">
+                                <input type="text" name="msg_subject" id="msg_subject" class="form-control" required
+                                    data-error="Please enter your subject" placeholder="Your Subject">
+                                <div class="help-block with-errors"></div>
+                            </div>
+                        </div>
+                        <div class="col-lg-12 col-md-12">
+                            <div class="form-group">
+                                <textarea name="message" class="form-control" id="message" cols="30" rows="5" required
+                                    data-error="Write your message" placeholder="Your Message"></textarea>
+                                <div class="help-block with-errors"></div>
+                            </div>
+                        </div>
+                        <div class="col-lg-12 col-md-12">
+                            <button type="submit" class="default-btn"><i
+                                    class='bx bx-paper-plane icon-arrow before'></i><span class="label">Send
+                                    Message</span><i class="bx bx-paper-plane icon-arrow after"></i></button>
+                            <div id="msgSubmit" class="h3 text-center hidden"></div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div id="particles-js-circle-bubble-3"></div>
+        <div class="contact-bg-image"><img src="assets/img/map.png" alt="image"></div>
+    </section>
+
+
+    <div id="map">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7004.14767792814!2d77.278489!3d28.627549000000002!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce35319b6a7ff%3A0x127dca80423ad527!2sLaxmi%20Nagar%2C%20Block%20S1%2C%20Nanakpura%2C%20Shakarpur%2C%20New%20Delhi%2C%20Delhi%20110092!5e0!3m2!1sen!2sin!4v1625470191841!5m2!1sen!2sin"></iframe>
+    </div>
+
+
+
+
+<?php include './templates/footer.php' ?>
+
+    <div class="go-top "><i class='bx bx-up-arrow-alt'></i></div>
+
+    <script data-cfasync="false " src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js "></script>
+    <script src="assets/js/jquery.min.js "></script>
+    <script src="assets/js/popper.min.js "></script>
+    <script src="assets/js/bootstrap.min.js "></script>
+    <script src="assets/js/owl.carousel.min.js "></script>
+    <script src="assets/js/mixitup.min.js "></script>
+    <script src="assets/js/parallax.min.js "></script>
+    <script src="assets/js/jquery.appear.min.js "></script>
+    <script src="assets/js/odometer.min.js "></script>
+    <script src="assets/js/particles.min.js "></script>
+    <script src="assets/js/meanmenu.min.js "></script>
+    <script src="assets/js/jquery.nice-select.min.js "></script>
+    <script src="assets/js/viewer.min.js "></script>
+    <script src="assets/js/slick.min.js "></script>
+    <script src="assets/js/jquery.magnific-popup.min.js "></script>
+    <script src="assets/js/jquery.ajaxchimp.min.js "></script>
+    <script src="assets/js/form-validator.min.js "></script>
+    <script src="assets/js/contact-form-script.js "></script>
+    <script src="assets/js/main.js "></script>
+</body>
+
+</html>
